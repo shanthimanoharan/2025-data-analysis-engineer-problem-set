f4f6d0fa3f097a2f59852fdc9013bc60402aef1c
Shanthi Manoharan <shanthi.jayaprakash@gmail.com>
2025-02-05 21:26:17 -0600

    solution 1

---
+++ solutions/sql_queries.sql
@@ -0,0 +1,7 @@
+1.select  client_id, 
+        month(time_id) as month,
+        count(distinct(user_id)) as users_num
+    from fact_events
+    group by client_id,
+             month(time_id)
+             
